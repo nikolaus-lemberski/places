0eeb1fec15a47b25dd014fcb16d2f0f98b7bfd29
Nikolaus Lemberski <nlemberski@pivotal.io>
2020-10-08 11:24:44 +0200

    Circuit Breaker logging

--- src/main/java/com/lemberski/demo/places/WeatherService.java
+++ src/main/java/com/lemberski/demo/places/WeatherService.java
@@ -1,6 +1,8 @@
 package com.lemberski.demo.places;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import java.util.Map;
 
 @Service
 public class WeatherService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WeatherService.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -24,7 +28,8 @@ public class WeatherService {
     }
 
     @SuppressWarnings("rawtypes")
-    public Map getWeatherFallback(String place) {
+    public Map getWeatherFallback(String place, Throwable t) {
+        LOG.warn("Cannot fetch weather data for place {}. Cause: {}", place, t.getMessage());
         return Map.of("locationName", place, "error", "Weather data not available");
     }
 
